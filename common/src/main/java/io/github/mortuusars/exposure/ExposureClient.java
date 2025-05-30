22d6027366594e470f0b9b9c59a599fa21b9d5b6
mortuusars <mortuusars@gmail.com>
2025-05-30 21:51:34 +0300

    todo

--- common/src/main/java/io/github/mortuusars/exposure/ExposureClient.java
+++ common/src/main/java/io/github/mortuusars/exposure/ExposureClient.java
@@ -82,6 +82,7 @@ public class ExposureClient {
     // --
 
     public static boolean shouldUseDirectCapture() {
+        //TODO: maybe check if neoforge is ok and then enable it only on fabric?
         if (PlatformHelper.isModLoaded("distanthorizons")
                 && (PlatformHelper.isModLoaded("oculus") || PlatformHelper.isModLoaded("iris"))) {
             return true;
