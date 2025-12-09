a9bd08b4f6a8444ca4ffb34d62ccf749d31f7865
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-12-09 16:48:18 -0500

    move files to common

--- forge/src/main/java/io/github/mortuusars/exposure/integration/jei/forge/ExposureJeiPluginImpl.java
+++ forge/src/main/java/io/github/mortuusars/exposure/integration/jei/forge/ExposureJeiPluginImpl.java
@@ -1,4 +1,4 @@
-package io.github.mortuusars.exposure.integration.jei.neoforge;
+package io.github.mortuusars.exposure.integration.jei.forge;
 
 //import com.simibubi.create.Create;
 //import com.simibubi.create.content.processing.sequenced.SequencedAssemblyRecipe;
@@ -6,8 +6,6 @@ package io.github.mortuusars.exposure.integration.jei.neoforge;
 //import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.registration.IRecipeRegistration;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public class ExposureJeiPluginImpl {
     public static void addSequencedDevelopingRecipes(@NotNull IRecipeRegistration registration) {
