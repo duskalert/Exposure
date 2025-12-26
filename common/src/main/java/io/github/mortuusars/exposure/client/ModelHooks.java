43147f3f863fa96904e915e84c90ed305d585c91
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-12-26 13:40:47 -0500

    fix overlay

--- common/src/main/java/io/github/mortuusars/exposure/client/ModelHooks.java
+++ common/src/main/java/io/github/mortuusars/exposure/client/ModelHooks.java
@@ -1,14 +1,20 @@
 package io.github.mortuusars.exposure.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
+import io.github.mortuusars.exposure.Config;
 import io.github.mortuusars.exposure.Exposure;
 import io.github.mortuusars.exposure.ExposureClient;
 import io.github.mortuusars.exposure.PlatformHelperClient;
+import io.github.mortuusars.exposure.client.camera.CameraClient;
+import io.github.mortuusars.exposure.client.camera.viewfinder.Viewfinder;
 import io.github.mortuusars.exposure.client.render.model.CameraModel;
 import io.github.mortuusars.exposure.client.util.Minecrft;
+import io.github.mortuusars.exposure.world.item.PhotographItem;
+import io.github.mortuusars.exposure.world.item.StackedPhotographsItem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.entity.LivingEntity;
@@ -40,5 +46,25 @@ public class ModelHooks {
         }
 
         return original;
+    }
+
+    public static boolean renderGui(GuiGraphics guiGraphics,float partialTick) {
+        Viewfinder viewfinder = CameraClient.viewfinder();
+        if (viewfinder != null && viewfinder.isLookingThrough()) {
+            viewfinder.overlay().render(guiGraphics,partialTick);
+            if (Config.Client.HIDE_HUD_WHILE_IN_VIEWFINDER.get()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean renderCrosshair() {
+        LocalPlayer player = Minecrft.player();
+        if (Config.Client.PHOTOGRAPH_IN_HAND_HIDE_CROSSHAIR.get() && player.getXRot() > 25f
+                && (player.getMainHandItem().getItem() instanceof PhotographItem || player.getMainHandItem().getItem() instanceof StackedPhotographsItem)
+                && player.getOffhandItem().isEmpty())
+            return true;
+        return false;
     }
 }
