db3ae79428fac8a1a69109943da58f9164bb2073
mortuusars <mortuusars@gmail.com>
2026-02-04 14:32:10 +0200

    Fixed Signed Album not being placeable in lectern

--- common/src/main/java/io/github/mortuusars/exposure/mixin/LecternBlockEntityMixin.java
+++ common/src/main/java/io/github/mortuusars/exposure/mixin/LecternBlockEntityMixin.java
@@ -20,5 +20,4 @@ public abstract class LecternBlockEntityMixin {
             cir.setReturnValue(true);
         }
     }
-
 }
--- common/src/main/java/io/github/mortuusars/exposure/world/item/SignedAlbumItem.java
+++ common/src/main/java/io/github/mortuusars/exposure/world/item/SignedAlbumItem.java
@@ -5,15 +5,21 @@ import io.github.mortuusars.exposure.Exposure;
 import io.github.mortuusars.exposure.client.gui.ClientGUI;
 import io.github.mortuusars.exposure.world.item.component.album.SignedAlbumContent;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.StringUtil;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LecternBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -47,6 +53,17 @@ public class SignedAlbumItem extends Item {
             ClientGUI.openAlbumViewScreen(player.getItemInHand(usedHand));
         }
         return InteractionResultHolder.success(player.getItemInHand(usedHand));
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(UseOnContext context) {
+        BlockPos blockPos = context.getClickedPos();
+        Level level = context.getLevel();
+        BlockState blockState = level.getBlockState(blockPos);
+        if (blockState.is(Blocks.LECTERN))
+            return LecternBlock.tryPlaceBook(context.getPlayer(), level, blockPos, blockState,
+                  context.getItemInHand()) ? InteractionResult.sidedSuccess(level.isClientSide) : InteractionResult.PASS;
+        return InteractionResult.PASS;
     }
 
     @Override
